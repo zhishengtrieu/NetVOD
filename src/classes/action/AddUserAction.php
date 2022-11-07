17c67b8691473525d4d9b3a365ba41b1f1163f56
Exelen <laura.gallion3@etu.univ-lorraine.fr>
2022-11-07 15:33:48 +0100

    Question 1 et 2, ajout d'un utilisateur et de pouvoir acceder a son compte

--- src/classes/action/AddUserAction.php
+++ src/classes/action/AddUserAction.php
@@ -5,14 +5,13 @@ use netvod\auth\Auth;
 
 class AddUserAction extends Action{
     public function execute(): string{
-        if($this->http_method == 'POST'){
+        if($this->http_method == 'POST') {
             $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-            $age = filter_var($_POST['age'], FILTER_SANITIZE_NUMBER_INT);
-            $genre = filter_var($_POST['genre'], FILTER_SANITIZE_STRING);
-            $res = "Email: $email, Age: $age ans, Genre musical: $genre";
+            $res = "";
             if (isset($_POST['email']) and isset($_POST['pwd'])){
                 if(Auth::register($_POST['email'], $_POST['pwd'])){
                     echo "L'utilisateur a bien été enregistré <br>";
+                    $res = "Email: $email";
                 }else{
                     echo "L'utilisateur n'a pas pu être enregistré <br>";
                 }
@@ -22,8 +21,6 @@ class AddUserAction extends Action{
             <form action="?action=add-user" method="POST">
             <input type="email" name="email" placeholder="Email">
             <input type="password" name="pwd" placeholder="password">
-            <input type="number" name="age" placeholder="Age">
-            <input type="text" name="genre" placeholder="Genre Musical">
             <input type="submit" value="Connexion">
             </form>
             END;
