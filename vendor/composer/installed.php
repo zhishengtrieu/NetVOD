82dd25d58e952d1943cb28a7e8f0c2758b401a0a
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-11-07 12:44:31 +0100

    ajout de l'autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '47d96d24038238e7ffa939482b4ea332a1cfcf43',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '47d96d24038238e7ffa939482b4ea332a1cfcf43',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
