279f289ffcb2d90afc972ae93b28ad56547c01c9
Hugoll-1 <101283981+Hugoll-1@users.noreply.github.com>
2022-11-07 15:20:33 +0100

    Ajout de la classe Serie avec constructeur, getteur et setteur

--- src/classes/video/serie/Serie.php
+++ src/classes/video/serie/Serie.php
@@ -5,7 +5,39 @@
 namespace netvod\video\Serie;
 use netvod\video\Episode;
 class Serie{
+
     private string $titre;
+    private string $genre;
+    private string $public;
+    private string $resume;
+    private string $dateSortieFilm;
+    private string $dateSortiePlateforme;
+    private int $nbEpisode;
+
+    public function __construct(string $titre, string $genre){
+        $this->titre = $titre;
+        $this->genre = $genre;
+    }
+
+    public function __set($attribut, $valeur){
+        if (property_exists($this, $attribut)){
+            if ($attribut == "titre" || $attribut == "genre" || $attribut == "public" || $attribut == "resume" || $attribut == "dateSortieFilm" || $attribut == "dateSortiePlateforme" || $attribut == "nbEpisode"){
+                $this->$attribut = $valeur;
+            }else{
+                throw new NonEditablePropertyException($attribut);
+            }
+        }else{
+            throw new InvalidPropertyNameException($attribut);
+        }
+    }
+
+    public function __get($attribut){
+        if (property_exists($this, $attribut)){
+            return $this->$attribut;
+        }else{
+            throw new InvalidPropertyNameException($attribut);
+        }
+    }
 
 }
 ?>
