539d6a7e13e330fdb907e04460740de7d3a03fdf
Hugoll-1 <101283981+Hugoll-1@users.noreply.github.com>
2022-11-09 14:43:01 +0100

    Ajout de la classe Commentaire.php

---
+++ src/classes/video/serie/Commentaire.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace netvod\video\serie;
+
+use netvod\exception\InvalidPropertyNameException;
+
+class Commentaire{
+
+    private string $commentaire;
+    private int $note;
+
+    public function __construct(string $comm, int $n){
+        $this -> commentaire = $comm;
+        $this -> note = $n;
+    }
+
+    public function __get($attribut){
+        if (property_exists($this, $attribut)){
+            return $this->$attribut;
+        }else{
+            throw new InvalidPropertyNameException($attribut);
+        }
+    }
+
+}
