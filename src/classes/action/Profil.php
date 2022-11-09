984bb624df75383a1da1c9f6db83267a4a903379
Exelen <laura.gallion3@etu.univ-lorraine.fr>
2022-11-09 17:04:53 +0100

    Ajout d'une page profil + header + ProfilRenderer;

--- src/classes/action/Profil.php
+++ src/classes/action/Profil.php
@@ -1,24 +1,53 @@
 <?php
 declare(strict_types=1);
+
 namespace netvod\action;
-class Profil extends Action{
+
+use netvod\db\ConnectionFactory;
+
+class Profil extends Action
+{
 
     public function execute(): string
     {
+        ConnectionFactory::makeConnection();
         $res = "";
-        if (isset($_SESSION['user'])){
-            $user = unserialize($_SESSION['user']);
-            $res .= "<h1>Profil</h1>";
-    }
 
-        else {
-            $res = <<<END
-            <form action="?action=add-user" method="POST">
+        if (isset($_SESSION['user'])) {
+            $user = unserialize($_SESSION['user'])->email;
+            var_dump($user);
+
+            if ($this->http_method == 'POST') {
+                if (isset($_POST['nom'])) {
+                    if ($_POST['nom'] !== "") {
+                        $nom = filter_var($_POST['nom'], FILTER_SANITIZE_STRING);
+                        $sql = ("update user set nom=? where email=?");
+                        $st = ConnectionFactory::$db->prepare($sql);
+                        $st->bindParam(1, $nom);
+                        $st->bindParam(2, $user);
+                        $st->execute();
+                    }
+                }
+                if (isset($_POST['prenom'])) {
+                    if ($_POST['prenom'] !== "") {
+                        $ui = filter_var($_POST['prenom'], FILTER_SANITIZE_STRING);
+                        $sqll = ("update user set prenom=? where email=?");
+                        $st = ConnectionFactory::$db->prepare($sqll);
+                        $st->bindParam(1, $ui);
+                        $st->bindParam(2, $user);
+                        $st->execute();
+                    }
+                }
+            } else {
+                $res = <<<END
+            <form action="?action=profil" method="POST">
             <input type="text" name="nom" placeholder="Rick">
             <input type="text" name="prenom" placeholder="Roll">
             <input type="submit" value="valider">
             </form>
             END;
+
+            }
 
         }
         return $res;
