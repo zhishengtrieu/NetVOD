0912af0730764c481112ea55c360afb255a922b8
melissandre88 <74959766+melissandre88@users.noreply.github.com>
2022-11-09 20:24:57 +0100

    ajout de la classe pour trier le catalogue par le nombre d'épisodes(celle avec le plus grand nombre en premiere)

---
+++ src/classes/video/tris/TriNbGrandEpisode.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace netvod\video\tris;
+
+class TriNbGrandEpisode implements Tri{
+
+    public function trier(): string{
+        $res = "";
+        $catalogue = new Catalogue();
+        $query = "select serie.id,serie.titre, count(episode.id)from serie 
+                inner join episode on serie.id = episode.serie_id 
+                group by serie.id,titre order by count(episode.id) desc";
+        $st = ConnectionFactory::$db->prepare($query);
+        $st->execute();
+        foreach ($st->fetchAll(PDO::FETCH_ASSOC) as $row) {
+            $id = $row['id'];
+            $titre = $row['titre'];
+            $serie = new Serie(intval($id),$titre);
+            $catalogue->ajouterSerie($serie);
+        }
+        $res .= (new CatalogueRenderer($catalogue))->render(Renderer::COMPACT);
+        return $res;
+    }
+}
