07cd61b81c3adb02c78df72b32aa11364774f586
Exelen <laura.gallion3@etu.univ-lorraine.fr>
2022-11-10 17:08:36 +0100

    Ajout d'un cas utilisateur dans ForgotPassword.php

--- src/classes/action/ForgotPassword.php
+++ src/classes/action/ForgotPassword.php
@@ -2,6 +2,7 @@
 
 namespace netvod\action;
 
+use netvod\auth\Auth;
 use netvod\db\ConnectionFactory;
 
 class ForgotPassword extends Action
@@ -12,23 +13,29 @@ class ForgotPassword extends Action
         if (isset($_COOKIE['kittie'])) {
             $sora = $_COOKIE['kittie'];
             if ($this->http_method == 'POST') {
-                $pwd=$_POST['pwd'];
-                if (($pwd !== '')&&(strlen($pwd) >= 10)) {
-                $db = ConnectionFactory::makeConnection();
-                $sql = ("update user set passwd =? where email=?");
-                $st = ConnectionFactory::$db->prepare($sql);
-                $var = $_GET['email'];
+                $pwd = $_POST['pwd'];
+                $mail = $_GET['email'];
+                if (($pwd !== '') && (strlen($pwd) >= 10)) {
+                    if (!Auth::emailLibre($mail)) {
+                    $db = ConnectionFactory::makeConnection();
+                    $sql = ("update user set passwd =? where email=?");
+                    $st = ConnectionFactory::$db->prepare($sql);
                     $password = password_hash($pwd, PASSWORD_DEFAULT);
-                $st->bindParam(1, $password);
-                $st->bindParam(2, $var);
-                $st->execute();
-                $res = "Vous avez changé de Mot de passe ";
-                setcookie('kittie', NULL, -1);
-            }else{
-                    $res="Mettez un vrai Mot de Passe";
-                } }else {
+                    $st->bindParam(1, $password);
+                    $st->bindParam(2, $mail);
+                    $st->execute();
+                    $res = "Vous avez changé de Mot de passe ";
+                    setcookie('kittie', NULL, -1);
+                } else{
+                        $res="Votre adresse mail n'est pas dans notre base de données";
+                    }
+                }
+                    else {
+                    $res = "Mettez un vrai Mot de Passe";
+                }
+            } else {
                 $var = $_GET['email'];
-                $res = <<<END
+                    $res = <<<END
             <form action="?action=$sora&email=$var" method="post">
                 <input type="password" name="pwd" placeholder="password">
                 <input type="submit" value="Valider">
