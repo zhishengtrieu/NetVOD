b27d6486f541157bfec78aef66a18755cb37206d
melissandre88 <74959766+melissandre88@users.noreply.github.com>
2022-11-10 08:50:48 +0100

    ajout du formulaire de commentaire au bon endroit

--- src/classes/action/AddCommentAction.php
+++ src/classes/action/AddCommentAction.php
@@ -2,9 +2,11 @@
 
 namespace netvod\action;
 
-class AddCommentAction extends Action{
+class AddCommentAction extends Action
+{
 
-    public function execute(): string{
+    public function execute(): string
+    {
         $res = "";
         if ($this->http_method == 'POST') {
             if (isset($_POST['commentaire']) and isset($_POST['note'])) {
@@ -21,31 +23,13 @@ class AddCommentAction extends Action{
                         $req = ConnectionFactory::$db->prepare("insert into commentaire (email, commentaire, note) values ($email, $commentaire, $note)");
                         $req->execute();
                         $res = "Commentaire ajoute<br>";
-                    }else{
+                    } else {
                         $res = "Vous avez deja commente <br>";
                     }
                     $_SESSION['user'] = serialize($user);
+                } else {
+                    $res .= 'Il faut se connecter avant d ajouter un commentaire';
                 }
-
-            } else {
-                $res = <<<END
-                <form action="?action=add-comment" method="POST">
-                    <input type="string" name="commentaire" placeholder="comment">
-                    <select name="note">
-                        <option value="1">1 etoile</option>
-                        <option value="1">1.5 etoile</option>
-                        <option value="2">2 etoiles</option>
-                        <option value="1">2.5 etoiles</option>
-                        <option value="3">3 etoiles</option>
-                        <option value="1">3.5 etoiles</option>
-                        <option value="4">4 etoiles</option>
-                        <option value="1">4.5 etoiles</option>
-                        <option value="5">5 etoiles</option>
-                        <option value="5">RickRoll etoiles</option>
-                    </select>
-                    <input type="submit" value="Enregistrer le commentaire">
-                </form>
-            END;
             }
         }
         return $res;
