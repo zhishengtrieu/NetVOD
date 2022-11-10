27eacc1bb0b6060200546fe56087642d820f952c
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-11-10 15:04:44 +0100

    modification catalogue

--- src/classes/render/CatalogueRenderer.php
+++ src/classes/render/CatalogueRenderer.php
@@ -14,20 +14,7 @@ class CatalogueRenderer implements Renderer{
         $this->catalogue = $catalogue;
     }
 
-    public function render(int $selector):string{
-        $html = "";
-        switch($selector){
-            case Renderer::COMPACT:
-                $html = $this->renderCompact();
-                break;
-            case Renderer::LONG:
-                $html = $this->renderLong();
-                break;
-        }
-        return $html;
-    }
-
-    private function renderCompact():string{
+    public function render(int $selector=Renderer::LONG):string{
         $html = "<h1>Catalogue</h1>";
         $html .= <<<HTML
             <form action="?action=rechercher" method="post">
@@ -48,11 +35,9 @@ class CatalogueRenderer implements Renderer{
                     <option value="8">Trier par date d'ajout decroissante</option>
                     <option value="9">Trier les séries de la plus ancienne à la plus récente</option>
                     <option value="10">Trier les séries de la plus récente à la plus ancienne</option>
-
-
                 </select>
-          <input type='submit' value='trier'>
-        </form>
+                <input type='submit' value='trier'>
+            </form>
         HTML;
         $html .= <<<HTML
             <form action="?action=filtrer-catalogue" method="post">
@@ -83,8 +68,4 @@ class CatalogueRenderer implements Renderer{
         return $html;
     }
 
-    private function renderLong():string{
-        $html = "";
-        return $html;
-    }
 }
