ba289bf1cafc5c8e56c8f9f0cda9c0f39fc8ad26
melissandre88 <74959766+melissandre88@users.noreply.github.com>
2022-11-10 17:22:47 +0100

    ajout des commentaires pour les tris par nombre d'épisodes

--- src/classes/video/tris/TriNbPetitEpisode.php
+++ src/classes/video/tris/TriNbPetitEpisode.php
@@ -11,20 +11,25 @@ use \PDO;
 
 class TriNbPetitEpisode implements Tri{
 
+    // cette fonction permettera de trier les series du catalogue par nombre d'episode du plus petit au plus grand
     public function trier(): string{
         $res = "";
+        //on créé un nouveau catalogue
         $catalogue = new Catalogue();
+        //requete sql permettant de recuperer l'id et le titre et le nombre d'episode et on tri du plus petit au plus grand
         $query = "select serie.id,serie.titre, count(episode.id)from serie 
                 inner join episode on serie.id = episode.serie_id 
                 group by serie.id,titre order by count(episode.id) asc";
         $st = ConnectionFactory::$db->prepare($query);
         $st->execute();
+        //pour chaque reponse à la requete sql(trier dans l'ordre decroissant) on creer une serie et on l'ajoute au catalogue
         foreach ($st->fetchAll(PDO::FETCH_ASSOC) as $row) {
             $id = $row['id'];
             $titre = $row['titre'];
             $serie = new Serie(intval($id),$titre);
             $catalogue->ajouterSerie($serie);
         }
+        //on affiche le catalogue et on le retourne
         $res .= (new CatalogueRenderer($catalogue))->render(Renderer::COMPACT);
         return $res;
     }
